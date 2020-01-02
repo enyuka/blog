1e533f15ab3811ec955ad823bb792d6894e33684
enyuka <nakamura345@gmail.com>
2020-01-02 23:53:30 +0900

    [Mod] WordpressのEnlighterっていうシンタックスハイライトのプラグインに合うようにmd2htmlを修正

--- md2html/main.js
+++ md2html/main.js
@@ -4,8 +4,10 @@ if (process.argv.length <= 2) {
 }
 
 const filepath = process.argv[2];
-const marked = require("marked");
 const fs = require("fs");
+const text = fs.readFileSync(filepath, "utf8");
+
+const marked = require("marked");
 marked.setOptions({
   renderer: new marked.Renderer(),
   // GitHub Flavored Markdownの書き方を有効にする
@@ -15,6 +17,14 @@ marked.setOptions({
   // h1とかにidが振られるのを無効にする
   headerIds: false,
 });
+let result = marked(text);
 
-const text = fs.readFileSync(filepath, "utf8");
-console.log(marked(text));
+// マークダウンのコードをWordpressのEnlighterが効くようにHTMLタグを置換する処理
+const regex1 = /<pre><code>/g;
+result = result.replace(regex1, '<pre class="EnlighterJSRAW" data-enlighter-language="generic">');
+const regex2 = /<\/code><\/pre>/g;
+result = result.replace(regex2, '</pre>');
+
+// クリップボードにコピー
+const clipboardy = require('clipboardy');
+clipboardy.writeSync(result);
